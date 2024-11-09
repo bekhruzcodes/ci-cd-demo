d980f2ee2b1dfd4d9c1185c995c1f7235be2cda6
bekhruzcodes <bekhruzbekmirzaliev744@gmail.com>
2024-11-09 17:28:53 +0500

    "Mission Accomplished"

--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 echo "Demo file for testing CI/CD";
 echo "PHP CI/CD Pipeline passed the test. Mission aomplished.";
 echo "Congrats. Keep practicing";
+
+echo "Problem came out because I tried out amend in different way";
